fd4a9a8da5cb46e01324370a142bfd824372cb10
shanhaider23 <shan.haider@gmail.com>
2025-02-20 13:03:24 +0100

    feat: added new pwa support

--- app/Components/Mapbox/Mapbox.js
+++ app/Components/Mapbox/Mapbox.js
@@ -1,17 +1,11 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
+import { MapContainer, TileLayer, useMapEvents, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useGlobalContext, useGlobalContextUpdate } from "@/app/context/globalContext";
-// import { MapContainer, TileLayer, useMapEvents, useMap } from 'react-leaflet';
 
-const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false });
-const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), { ssr: false });
-const useMapEvents = dynamic(() => import("react-leaflet").then((mod) => mod.useMapEvents), { ssr: false });
-const useMap = dynamic(() => import("react-leaflet").then((mod) => mod.useMap), { ssr: false });
-
-// Dynamic import for Leaflet to avoid SSR issues
+// Import Leaflet dynamically to avoid SSR issues
 const L = typeof window !== "undefined" ? require("leaflet") : null;
 
 function Mapbox() {
@@ -19,14 +13,12 @@ function Mapbox() {
   const { setActiveCityCoords } = useGlobalContextUpdate();
   const activeCityCords = forecast?.coord;
 
-  // State to hold the map instance and weather data
   const [map, setMap] = useState(null);
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState('');
   const [description, setDescription] = useState('');
 
   useEffect(() => {
-    // Set city and weather data when forecast changes
     if (forecast && forecast.main && forecast.weather) {
       setCity(forecast.name);
       setWeather(`${Math.round(forecast.main.temp - 273.15)}°C`);
@@ -36,12 +28,10 @@ function Mapbox() {
 
   useEffect(() => {
     if (map && activeCityCords) {
-      const zoomLev = 13;
-      const flyToOptions = {
+      map.flyTo([activeCityCords.lat, activeCityCords.lon], 13, {
         animate: true,
         duration: 1.5,
-      };
-      map.flyTo([activeCityCords.lat, activeCityCords.lon], zoomLev, flyToOptions);
+      });
     }
   }, [map, activeCityCords]);
 
@@ -49,7 +39,6 @@ function Mapbox() {
     return <div><h1>Loading...</h1></div>;
   }
 
-  // Custom component to handle map click events
   const MapClickHandler = () => {
     const map = useMap();
 
@@ -58,20 +47,14 @@ function Mapbox() {
         const { lat, lng } = e.latlng;
         setActiveCityCoords([lat, lng]);
 
-        if (L) { // Check if Leaflet is available
-          const popupContent = forecast && forecast.main && forecast.weather
-            ? `
-              <div class='map-box__popup-details'>
-                <h1>${city || 'Unknown location'}</h1>
-                <h2>Temperature: ${weather}</h2>
-                <h2>Condition: ${description || 'No data'}</h2>
-              </div>
-            `
-            : `
-              <div class='map-box__popup-details'>
-                <h1>Loading weather data...</h1>
-              </div>
-            `;
+        if (L) {
+          const popupContent = `
+            <div class='map-box__popup-details'>
+              <h1>${city || 'Unknown location'}</h1>
+              <h2>Temperature: ${weather}</h2>
+              <h2>Condition: ${description || 'No data'}</h2>
+            </div>
+          `;
 
           L.popup()
             .setLatLng([lat, lng])
@@ -84,10 +67,10 @@ function Mapbox() {
     return null;
   };
 
-
   return (
-    <div className="flex-1 basis-[50%] border rounded-lg ">
-      <h1 className="text-xl pt-2  pb-2 text-center">Tap on the Map for Local Weather</h1>
+    <div className="flex-1 basis-[50%] border rounded-lg">
+      <h1 className="text-sm sm:text-xl pt-2 pb-2 text-center">Tap on the Map for Local Weather</h1>
+
       <MapContainer
         center={[activeCityCords.lat, activeCityCords.lon]}
         zoom={13}
